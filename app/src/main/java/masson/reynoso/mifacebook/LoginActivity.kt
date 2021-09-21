455a3706e67f9cef250386ef3ada2cb4a1fb19ce
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-09-20 20:52:00 -0700

    último commit

--- app/src/main/java/masson/reynoso/mifacebook/LoginActivity.kt
+++ app/src/main/java/masson/reynoso/mifacebook/LoginActivity.kt
@@ -1,11 +1,21 @@
 package masson.reynoso.mifacebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn_crear_cuenta: Button = findViewById(R.id.btn_crear_cuenta)
+
+        btn_crear_cuenta.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
